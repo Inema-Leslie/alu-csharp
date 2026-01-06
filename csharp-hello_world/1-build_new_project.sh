ba3d1a1fbdd67d1f31fc469f777af7480cefe7b0
Inema_Leslie <a.inema2@alustudent.com>
2026-01-06 10:17:37 +0200

    initializing a new C# project

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-# Create project
-dotnet new console -o 1-new_project > /dev/null 2>&1
-
-# Build and capture/output the result
-cd 1-new_project
-dotnet build
+dotnet new console -o 1-new_project && cd 1-new_project && dotnet build
