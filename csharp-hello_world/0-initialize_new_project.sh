dcf9262dba06a5a3450cfe4273b5bca1735d7497
Inema_Leslie <a.inema2@alustudent.com>
2026-01-06 10:21:16 +0200

    initializing a new C# project

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-
-# Ensure we're in the right location
-mkdir -p 0-new_project
-
-# Use the full path approach
-dotnet new console --output ./0-new_project --name 0-new_project --force
+dotnet new console -o 0-new_project
