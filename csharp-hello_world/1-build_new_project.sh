692fa04b64b48d67ccca36b39523847e2357f386
Inema_Leslie <a.inema2@alustudent.com>
2026-01-06 10:01:23 +0200

    initializing a new C# project

---
+++ csharp-hello_world/1-build_new_project.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Create and initialize a new C# console project in 1-new_project folder
+dotnet new console -o 1-new_project
+
+# Build the project
+dotnet build 1-new_project
