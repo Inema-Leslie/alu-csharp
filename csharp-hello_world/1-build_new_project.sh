653a4a9e2e645db14018ed031c1ef814d43c994d
Inema_Leslie <a.inema2@alustudent.com>
2026-01-08 14:47:56 +0200

    initializing and building a c# project

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-mkdir -p 1-new_project
-cd 1-new_project
-echo '{
-  "sdk": {
-    "version": "7.0.120",
-    "rollForward": "disable"
-  }
-}' > global.json
-dotnet new console --name 1-new_project
-dotnet restore
-dotnet build
-cd ..
+dotnet new console -o 1-new_project --force
+dotnet build 1-new_project
