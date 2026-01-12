ce9826a6e43b7a4088963f5491a3a7a5d6c2db42
Inema_Leslie <a.inema2@alustudent.com>
2026-01-12 14:19:38 +0200

    2-new_project

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p 2-new_project
-cd 2-new_project
-dotnet new console 
-dotnet build 
-dotnet 
-
+cd 2-new_project 2>/dev/null
+dotnet new console > /dev/null 2>&1
+dotnet build > /dev/null 2>&1
+dotnet run 2>&1 | grep "Hello"
