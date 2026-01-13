ea456cbb5a043e772aaa6360de4db19e0980ed32
Inema_Leslie <a.inema2@alustudent.com>
2026-01-13 13:56:14 +0200

    2-new project

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-rm -rf 1-new_project 2>/dev/null
-mkdir -p 1-new_project
-cd 1-new_project || exit 1
-
-dotnet new console --framework net7.0 > /dev/null 2>&1
-
-dotnet restore > /dev/null 2>&1
-dotnet build 2>&1 | grep -A 2 "Build succeeded"
-
-cd ..
+dotnet new console -o 1-new_project
+cd 1-new_project
+dotnet build
