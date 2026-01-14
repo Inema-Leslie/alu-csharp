53759b8c038f367478cf8e014bf2414a40f2c90d
Inema_Leslie <a.inema2@alustudent.com>
2026-01-14 10:52:26 +0200

    writeline

---
+++ csharp-hello_world/3-writeline.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WritelineExercise
+{
+    class WritelineProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
