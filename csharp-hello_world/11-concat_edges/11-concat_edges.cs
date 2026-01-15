2709fd0ef0fa8b0a4731b0b3d17d298867915d46
Inema_Leslie <a.inema2@alustudent.com>
2026-01-15 12:17:23 +0200

    11

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -4,7 +4,8 @@ class Program
     static void Main(string[] args)
     {
         string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-        str = str.Substring(str.IndexOf('o'), str.IndexOf('-') - str.IndexOf('o') + 10) + " " + str.Substring(str.IndexOf('p', str.IndexOf('-')), str.IndexOf(' ', str.IndexOf('p', str.IndexOf('-'))) - str.IndexOf('p', str.IndexOf('-'))) + " " + str.Substring(str.IndexOf('i', str.IndexOf('.', str.IndexOf('p', str.IndexOf('-')))), 2) + " " + str.Substring(0, 2);
+        char sp=str[1]; int o=str.IndexOf('o'), d=str.IndexOf('-'), p=str.IndexOf('p',d), s=str.IndexOf(sp,p), i=str.IndexOf('i',str.IndexOf('.',p));
+        str=str.Substring(o,d-o+10)+sp+str.Substring(p,s-p)+sp+str.Substring(i,2)+sp+str.Substring(0,2);
         Console.WriteLine(str);
     }
 }
