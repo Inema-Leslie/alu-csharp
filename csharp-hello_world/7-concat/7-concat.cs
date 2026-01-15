3cf97c30cf445f59e44cb2d004d9149d2c790643
Inema_Leslie <a.inema2@alustudent.com>
2026-01-15 10:12:09 +0200

    hoberlton

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Holberton";
+        string str2 = "School";
+        string message = "Welcome to " + str1 + " " + str2 + "!";
+        Console.WriteLine(message);
+    }
+}
