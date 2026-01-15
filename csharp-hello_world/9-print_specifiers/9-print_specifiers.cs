09cf6f8e7dd8aa60f67051efc390801cd51e1822
Inema_Leslie <a.inema2@alustudent.com>
2026-01-15 10:38:12 +0200

    9

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+        Console.WriteLine($"Percent: {percent:P2}\nCurrency: {currency:C}");
+    }
+}
