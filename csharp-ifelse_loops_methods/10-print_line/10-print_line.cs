43b705973fb7652984fc2f8f38f3476bf895dd3d
Inema_Leslie <a.inema2@alustudent.com>
2026-01-16 12:00:12 +0200

    if else loops

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("_");
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Line.PrintLine(3);
+        Line.PrintLine(0);
+        Line.PrintLine(12);
+        Line.PrintLine(-98);
+    }
+}
