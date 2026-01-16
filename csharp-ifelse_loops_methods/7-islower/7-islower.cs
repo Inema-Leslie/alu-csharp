d4cf75d2520461155a09bfd9bbc1172db8dbb1dc
Inema_Leslie <a.inema2@alustudent.com>
2026-01-16 11:48:25 +0200

    if else loops

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -4,8 +4,9 @@ class Character
 {
     public static bool IsLower(char c)
     {
-        // Check if character is between 'a' and 'z' in ASCII
-        return c >= 'a' && c <= 'z';
+        
+        int asciiValue = (int)c;
+        return asciiValue >= 97 && asciiValue <= 122;
     }
 }
 
@@ -14,9 +15,8 @@ class Program
     static void Main(string[] args)
     {
         char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g'};
-        int i;
-
-        for (i = 0; i < letters.Length; i++)
+        
+        for (int i = 0; i < letters.Length; i++)
         {
             if(Character.IsLower(letters[i]))
                 Console.WriteLine("{0} is lowercase", letters[i]);
