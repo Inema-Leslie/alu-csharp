2c1256f2c52648724068c300010eee14017ebde1
Inema_Leslie <a.inema2@alustudent.com>
2026-01-16 10:33:54 +0200

    if else loops

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(-10, 10);
+        
+        if (number > 0)
+        {
+            Console.WriteLine($"{number} is positive");
+        }
+        else if (number == 0)
+        {
+            Console.WriteLine($"{number} is zero");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is negative");
+        }
+    }
+}
