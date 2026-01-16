d8325c9e5ff8bf93abbb09f485e04b16e112175a
Inema_Leslie <a.inema2@alustudent.com>
2026-01-16 11:10:15 +0200

    if else loops

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string alphabet = "";
+        
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            alphabet += letter;
+        }
+        
+        Console.Write(alphabet);
+    }
+}
