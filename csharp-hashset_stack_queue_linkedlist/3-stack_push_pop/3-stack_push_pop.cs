e398d9282a2f9881d9deae36d0db307446f81c43
Inema_Leslie <a.inema2@alustudent.com>
2026-01-22 17:45:22 +0200

    queues and stacks

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,67 +5,40 @@ public static class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
+        // Print number of items
         Console.WriteLine("Number of items: {0}", aStack.Count);
         
-        if (aStack.Count == 0)
-        {
-            Console.WriteLine("Stack is empty");
-        }
-        else
-        {
+        // Print top item
+        if (aStack.Count > 0)
             Console.WriteLine("Top item: {0}", aStack.Peek());
-        }
+        else
+            Console.WriteLine("Stack is empty");
         
-        bool containsSearch = aStack.Contains(search);
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+        // Check if contains search
+        bool contains = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, contains);
         
-        if (containsSearch && aStack.Count > 0)
+        // If contains search, remove items up to and including it
+        if (contains)
         {
-            // We need to remove items up to and including search
-            // We can only use Pop() once, so we need a different approach
-            
-            // Create a new stack to rebuild
-            Stack<string> newStack = new Stack<string>();
-            bool found = false;
-            
-            // Move items from aStack to newStack until we find search
-            // We'll use ONE Pop() in a loop, but that's multiple calls...
-            // Actually, we need to avoid Pop() entirely
-            
-            // Alternative: Use an array
-            string[] items = new string[aStack.Count];
-            aStack.CopyTo(items, 0);
-            Array.Reverse(items); // Now items[0] is top of stack
-            
+            // Get all items as array
+            string[] items = aStack.ToArray();
             aStack.Clear();
             
-            // Find search and rebuild
-            bool keep = false;
-            foreach (string item in items)
-            {
-                if (item == search)
-                {
-                    keep = true; // Start keeping items AFTER search
-                    continue; // Don't add search itself
-                }
-                
-                if (keep)
-                {
-                    aStack.Push(item);
-                }
-            }
+            // Find search index (array is from bottom to top)
+            int index = Array.IndexOf(items, search);
             
-            // Now we need to reverse the order
-            // The items we pushed are in wrong order
-            Stack<string> temp = new Stack<string>();
-            while (aStack.Count > 0)
+            // Push items that were below search in original stack
+            // (items with index < searchIndex in the array)
+            for (int i = index - 1; i >= 0; i--)
             {
-                temp.Push(aStack.Pop()); // This uses Pop()! We need to avoid
+                aStack.Push(items[i]);
             }
-            aStack = temp;
         }
         
+        // Add new item
         aStack.Push(newItem);
+        
         return aStack;
     }
 }
