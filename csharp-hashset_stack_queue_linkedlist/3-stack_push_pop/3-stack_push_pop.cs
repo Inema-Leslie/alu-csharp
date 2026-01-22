4cb496edc9930cf68c1edcfe0ad346e803b0acb3
Inema_Leslie <a.inema2@alustudent.com>
2026-01-22 17:37:25 +0200

    queues and stacks

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,10 +5,8 @@ public static class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        // Print number of items
         Console.WriteLine("Number of items: {0}", aStack.Count);
         
-        // Print top item or "Stack is empty"
         if (aStack.Count == 0)
         {
             Console.WriteLine("Stack is empty");
@@ -18,52 +16,36 @@ public static class MyStack
             Console.WriteLine("Top item: {0}", aStack.Peek());
         }
         
-        // Print if stack contains search item
         bool containsSearch = aStack.Contains(search);
         Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
         
-        // If stack contains search, remove all items up to and including search
         if (containsSearch)
         {
-            // Create a new stack to hold items we want to keep
-            Stack<string> tempStack = new Stack<string>();
-            bool found = false;
+            // Convert stack to array to avoid multiple Pop() calls
+            string[] array = aStack.ToArray();
             
-            // We can only use Pop() once per iteration, so we need to rebuild the stack
-            while (aStack.Count > 0)
+            // Clear the stack
+            aStack.Clear();
+            
+            // Rebuild the stack, skipping items until we find search
+            bool found = false;
+            for (int i = array.Length - 1; i >= 0; i--)
             {
-                string item = aStack.Pop(); // This is our ONE Pop() call per iteration
-                
-                if (!found && item == search)
+                if (!found && array[i] == search)
                 {
                     found = true;
-                    // Don't add this item (search item) to tempStack
-                    continue;
+                    continue; // Skip the search item
                 }
                 
-                if (!found)
+                if (found)
                 {
-                    // Items above the search (haven't found it yet)
-                    // Don't add them since we're removing everything up to search
-                    continue;
+                    // Items after the search item
+                    aStack.Push(array[i]);
                 }
-                else
-                {
-                    // Items below the search (after we found it)
-                    tempStack.Push(item);
-                }
-            }
-            
-            // Rebuild the original stack with items after search
-            while (tempStack.Count > 0)
-            {
-                aStack.Push(tempStack.Pop());
             }
         }
         
-        // Add new item to stack
         aStack.Push(newItem);
-        
         return aStack;
     }
 }
