90f32b7f15c95e91b8c4d0ad465bc6044f12a7f3
Inema_Leslie <a.inema2@alustudent.com>
2026-03-09 10:31:31 +0200

    inheritance

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+using System;
+
+/// <summary>
+/// Empty base class Animal
+/// </summary>
+public class Animal
+{
+    // Empty class
+}
+
+/// <summary>
+/// Empty derived class Dog that inherits from Animal
+/// </summary>
+public class Dog : Animal
+{
+    // Empty class
+}
